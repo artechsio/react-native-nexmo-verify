f2fdcb7d8798d171fd7de7a3b5ccaae2ce50c2db
asanska <asanska@hotmail.com>
2016-08-03 23:49:51 +0300

    android_initial_function

--- android/src/main/java/io/artechs/rn/nexmoverify/Config.java
+++ android/src/main/java/io/artechs/rn/nexmoverify/Config.java
@@ -4,6 +4,6 @@ package io.artechs.rn.nexmoverify;
  * Created by emps on 3 Aug 2016.
  */
 public class Config {
-    public static final String AppId = "YourAppID";
-    public static final String SharedSecretKey = "YourSharedSecret";
+    public static final String AppId = "458bd1c9";
+    public static final String SharedSecretKey = "3e6ee7d7440fe08e";
 }
